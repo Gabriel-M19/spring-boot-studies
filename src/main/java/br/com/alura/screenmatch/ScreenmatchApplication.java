6b35979bcf6703df151cee7f353d6cb0ddf6af19
gabri <gabrieldemelo7399@gmail.com>
2026-04-17 02:39:30 -0300

    first commit

---
+++ src/main/java/br/com/alura/screenmatch/ScreenmatchApplication.java
@@ -0,0 +1,29 @@
+package br.com.alura.screenmatch;
+
+import br.com.alura.screenmatch.Service.ConsumoApi;
+import br.com.alura.screenmatch.Service.ConverteDados;
+import br.com.alura.screenmatch.model.DadosSerie;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScreenmatchApplication implements CommandLineRunner {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScreenmatchApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception{
+		var consumoAPI = new ConsumoApi();
+		var json = consumoAPI.obterDados("https://www.omdbapi.com/?t=gilmore+girls&1&apikey=a854f65e");
+//		System.out.println(json);
+//		json = consumoAPI.obterDados("https://coffee.alexflipnote.dev/random.json");
+		System.out.println(json);
+		ConverteDados conversor = new ConverteDados();
+		DadosSerie dados = conversor.obterDados(json, DadosSerie.class);
+		System.out.println(dados);
+	}
+
+}
