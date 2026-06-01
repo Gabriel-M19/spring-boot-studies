8110ed3d883cbb32c320daf9bc88ddafbe8b5288
gabri <gabrieldemelo7399@gmail.com>
2026-06-01 16:11:27 -0300

    .

--- src/main/java/br/com/alura/screenmatch/model/Categoria.java
+++ src/main/java/br/com/alura/screenmatch/model/Categoria.java
@@ -1,43 +1,32 @@
 package br.com.alura.screenmatch.model;
 
 public enum Categoria {
-    ACAO("Action"),
-    AVENTURA("Adventure"),
-    ANIMACAO("Animation"),
-    COMEDIA("Comedy"),
-    COMEDIA_ROMANTICA("Romantic Comedy"),
-    CRIME("Crime"),
-    DOCUMENTARIO("Documentary"),
-    DRAMA("Drama"),
-    ESPORTE("Sport"),
-    FANTASIA("Fantasy"),
-    FAROESTE("Western"),
-    FICCAO_CIENTIFICA("Sci-Fi"),
-    GUERRA("War"),
-    HISTORICO("History"),
-    MISTERIO("Mystery"),
-    MUSICAL("Musical"),
-    POLICIAL("Crime"),
-    ROMANCE("Romance"),
-    SUSPENSE("Thriller"),
-    TERROR("Horror"),
-    THRILLER("Thriller"),
-    BIOGRAFIA("Biography"),
-    FAMILIA("Family"),
-    INFANTIL("Children"),
-    NOIR("Film-Noir"),
-    REALITY_SHOW("Reality-TV");
-
+    ACAO("Action", "Ação"),
+    ROMANCE("Romance", "Romance"),
+    COMEDIA("Comedy", "Comédia"),
+    DRAMA("Drama", "Drama"),
+    CRIME("Crime", "Crime");
 
     private String categoriaOmdb;
+    private String categoriaPortugues;
 
-    Categoria(String categoriaOmdb){
+    Categoria(String categoriaOmdb, String categoriaPortugues){
         this.categoriaOmdb = categoriaOmdb;
+        this.categoriaPortugues = categoriaPortugues;
     }
 
     public static Categoria fromString(String text) {
         for (Categoria categoria : Categoria.values()) {
             if (categoria.categoriaOmdb.equalsIgnoreCase(text)) {
+                return categoria;
+            }
+        }
+        throw new IllegalArgumentException("Nenhuma categoria encontrada para a string fornecida: " + text);
+    }
+
+    public static Categoria fromPortugues(String text) {
+        for (Categoria categoria : Categoria.values()) {
+            if (categoria.categoriaPortugues.equalsIgnoreCase(text)) {
                 return categoria;
             }
         }
--- src/main/java/br/com/alura/screenmatch/model/Serie.java
+++ src/main/java/br/com/alura/screenmatch/model/Serie.java
@@ -1,5 +1,6 @@
 package br.com.alura.screenmatch.model;
 
+import br.com.alura.screenmatch.service.ConsultaChatGPT;
 import jakarta.persistence.*;
 
 import java.util.ArrayList;
@@ -9,37 +10,28 @@ import java.util.OptionalDouble;
 @Entity
 @Table(name = "series")
 public class Serie {
-
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-
-    @Column(unique = true)
     private String titulo;
-
     private Integer totalTemporadas;
-
     private Double avaliacao;
-
     @Enumerated(EnumType.STRING)
     private Categoria genero;
-
     private String atores;
-
     private String poster;
-
     private String sinopse;
 
-    @Transient
-    private List<Episodio> epsiodio = new ArrayList<>();
+    @OneToMany(mappedBy = "serie", cascade = CascadeType.ALL, fetch = FetchType.EAGER)
+    private List<Episodio> episodios = new ArrayList<>();
 
-    public Serie(){}
+    public Serie() {}
 
     public Serie(DadosSerie dadosSerie){
         this.titulo = dadosSerie.titulo();
         this.totalTemporadas = dadosSerie.totalTemporadas();
         this.avaliacao = OptionalDouble.of(Double.valueOf(dadosSerie.avaliacao())).orElse(0);
-        this.genero = Categoria.fromString(dadosSerie.genero().split(",") [0].trim());
+        this.genero = Categoria.fromString(dadosSerie.genero().split(",")[0].trim());
         this.atores = dadosSerie.atores();
         this.poster = dadosSerie.poster();
         this.sinopse = dadosSerie.sinopse();
@@ -53,12 +45,13 @@ public class Serie {
         this.id = id;
     }
 
-    public List<Episodio> getEpsiodio() {
-        return epsiodio;
+    public List<Episodio> getEpisodios() {
+        return episodios;
     }
 
-    public void setEpsiodio(List<Episodio> epsiodio) {
-        this.epsiodio = epsiodio;
+    public void setEpisodios(List<Episodio> episodios) {
+        episodios.forEach(e -> e.setSerie(this));
+        this.episodios = episodios;
     }
 
     public String getTitulo() {
@@ -119,14 +112,14 @@ public class Serie {
 
     @Override
     public String toString() {
-        return '{' +
-                "genero: " + genero +
-                ", titulo: " + titulo +
-                ", totalTemporadas: " + totalTemporadas +
-                ", avaliacao: " + avaliacao +
-                ", atores: " + atores +
-                ", poster: " + poster +
-                ", sinopse: " + sinopse +
-                '}';
+        return
+                "genero=" + genero +
+                        ", titulo='" + titulo + '\'' +
+                        ", totalTemporadas=" + totalTemporadas +
+                        ", avaliacao=" + avaliacao +
+                        ", atores='" + atores + '\'' +
+                        ", poster='" + poster + '\'' +
+                        ", sinopse='" + sinopse + '\'' +
+                        ", episodios='" + episodios + '\'';
     }
 }
